27369069da1b8b0d4663cdf1bf8fa5a12f2615ad
Galaxy-ANVIP <71951621+Galaxy-ANVIP@users.noreply.github.com>
2020-12-13 14:17:17 +0700

    Create play.js

---
+++ commands/play.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "Play",
+  aliases: ["p"],
+  run: async (client, message, args) => {
+    const musik = args.join(" ") 
+    if(!musik) return message.channel.send("Masukan nama musik")
+    
+    client.player.play(message, musik)
+   }
+}
+© 2020 GitHub
